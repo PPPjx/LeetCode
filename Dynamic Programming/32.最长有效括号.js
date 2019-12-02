2dd9d9394598bdc0bbb44950d05adf4212670deb
jiaxin.pan <782713997@qq.com>
2019-12-02 16:06:11 +0800

    2019-12-02

---
+++ Dynamic Programming/32.最长有效括号.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=javascript
+ *
+ * [32] 最长有效括号
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+//动态规划 存储每个位置匹配最长有效括号
+var longestValidParentheses = function(s) {
+    let arr = [];
+    let maxLength = 0, left = 0;
+    for(let i = 0; i < s.length; ++i) {
+        arr[i] = 0;
+        if(s[i] == '(') {
+            left++;
+            arr[i] = 0;
+        }
+        if(s[i] == ')' && left > 0) {
+            arr[i] = arr[i - 1] + 2;
+            left--;
+            if(i - arr[i] >= 0)
+                arr[i] += arr[i - arr[i]];
+        }
+        if(arr[i] > maxLength)
+            maxLength = arr[i];
+    }
+    return maxLength;
+};
+
