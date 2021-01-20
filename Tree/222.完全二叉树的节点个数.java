b0b4b037edcdc6f69db308256d5aa826d1f5d050
pppjx <782713997@qq.com>
2021-01-20 09:45:17 +0800

    2021-01-20 222.完全二叉树的节点个数

---
+++ Tree/222.完全二叉树的节点个数.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=java
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        // 老老实实递归他不香吗
+        if (root == null) {
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left + right + 1;
+    }
+}
+// @lc code=end
+
