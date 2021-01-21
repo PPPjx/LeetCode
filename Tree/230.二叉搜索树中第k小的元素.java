e43043a968f25f822ace1fff714b78e63be68774
pppjx <782713997@qq.com>
2021-01-21 13:48:44 +0800

    2021-01-21 230.二叉搜索树中第k小的元素

---
+++ Tree/230.二叉搜索树中第k小的元素.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=java
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        // 二叉搜索树的中序遍历 是从小到大排列的
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // 中间插入一步判断是否是第k小
+            if(--k == 0) return root.val;
+            root = root.right;
+        }
+        return 0;
+    }
+}
+// @lc code=end
+
