5e9647583634d433ebe20d778133a0f12ce602c3
pppjx <782713997@qq.com>
2021-02-05 13:53:23 +0800

    2021-02-05 509.斐波那契数

---
+++ Tree/509.斐波那契数.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=java
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+class Solution {
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
+// @lc code=end
+
